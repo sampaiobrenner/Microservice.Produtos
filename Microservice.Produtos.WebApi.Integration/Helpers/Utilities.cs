b4d68a2ba68f67a3c45ae8ae68b4e118bd72490f
Matheus Sampaio <sampaio.brenner@gmail.com>
2020-03-03 20:03:05 -0300

    Adicionando polly - Caso não consiga criar o banco tenta por 3x a cada 30s

--- Microservice.Produtos.WebApi.Integration/Helpers/Utilities.cs
+++ Microservice.Produtos.WebApi.Integration/Helpers/Utilities.cs
@@ -1,7 +1,8 @@
-using System.Collections.Generic;
 using Microservice.Produtos.Entities.Entities;
 using Microservice.Produtos.Entities.Entities.Builders;
 using Microservice.Produtos.Repositories.Contexts;
+using System;
+using System.Collections.Generic;
 
 namespace Microservice.Produtos.WebApi.IntegrationTests.Helpers
 {
@@ -10,12 +11,21 @@ namespace Microservice.Produtos.WebApi.IntegrationTests.Helpers
         public static List<Produto> GetProdutosParaTestes()
         {
             var produto1 = new ProdutoBuilder()
+                .WithId(Guid.NewGuid())
                 .WithNome("Produto teste 1")
                 .Build();
 
             var produto2 = new ProdutoBuilder()
-               .WithNome("Produto teste 2")
-               .Build();
+                .WithId(Guid.NewGuid())
+                .WithNome("Produto teste 2")
+                .Build();
+
+            var categoria = new CategoriaDoProdutoBuilder()
+                .WithId(Guid.NewGuid())
+                .WithNome("Categoria teste")
+                .Build();
+
+            produto2.SetCategoriaDoProduto(categoria);
 
             return new List<Produto>() { produto1, produto2 };
         }
